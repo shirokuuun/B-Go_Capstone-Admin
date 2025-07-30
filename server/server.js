a770ea129315f75d56d44373bfa15ca44339d340
Marcus Neo Rangel <marcusneorangel23@gmail.com>
2025-07-30 23:13:06 +0800

    Initial Commit for the admin repo

---
+++ server/server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const corsOptions = {
+  origin: "http://localhost:5173",
+};
+
+app.use(cors(corsOptions));
+
+app.get("/api", (req, res) => {
+  res.json({ fruits: ["Apple", "strawberry", "Cherry"] });
+});
+
+app.listen(3000, () => {
+  console.log("Server is runndwaing on port 3000");
+});
